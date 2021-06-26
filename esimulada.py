893be3bf1ac1180a0b75c5062a829bd0e8f9e7e2
erika0916 <eklissth@gmail.com>
2021-06-26 02:13:23 -0500

    Se agrega color de fondo del frame y distintas tonalidades a los Labels

--- esimulada.py
+++ esimulada.py
@@ -23,7 +23,7 @@ ventanaes.geometry("500x600")
 framePadre = LabelFrame(ventanaes)
 
 #Creación del Canvas
-mycanvas = Canvas(framePadre, height=500, width=400)
+mycanvas = Canvas(framePadre, height=500, width=400, background="white")
 mycanvas.pack(side=LEFT, fill="both", expand="yes")
 
 #Creación del Scrollbar
@@ -33,54 +33,54 @@ yscrollbar.pack(side=RIGHT, fill="y")
 mycanvas.configure(yscrollcommand=yscrollbar.set)
 mycanvas.bind('<Configure>', lambda e: mycanvas.configure(scrollregion=mycanvas.bbox('all')))
 
-myframe = Frame(mycanvas, height=700, width=500)
+myframe = Frame(mycanvas, height=700, width=500, background="white")
 mycanvas.create_window((0,0), window=myframe, anchor="nw")
 
 #Posicionamiento en pantalla del LabelFrame
 framePadre.pack(fill="both", expand="yes", padx=2, pady=2)
 
-tituloVentanaSimulada = tk.Label(myframe, text="Estructura simulada", font="Roboto 18 bold",background="white")
+tituloVentanaSimulada = tk.Label(myframe, text="Estructura simulada", font="Roboto 18 bold", foreground="#032e42", background="white")
 tituloVentanaSimulada.place(x=130, y=0)
 
-nombreArchivo = tk.Label(myframe, text="Nombre del archivo:", font="Roboto 12", background="white")
+nombreArchivo = tk.Label(myframe, text="Nombre del archivo:", font="Roboto 12", foreground="#012e67", background="white")
 nombreArchivo.place(x=70, y=50)
 datoNombreArchivo = tk.Label(myframe, text="xxxxxx", font="Roboto 12", background="white")
 datoNombreArchivo.place(x=350, y=50)
 
-anchoBanda = tk.Label(myframe, text="Ancho de banda para la frecuencia:", font="Roboto 12", background="white")
+anchoBanda = tk.Label(myframe, text="Ancho de banda para la frecuencia:", font="Roboto 12", foreground="#193439", background="white")
 anchoBanda.place(x=70, y=90)
 datoAnchoBanda = tk.Label(myframe, text="xxxxxx", font="Roboto 12", background="white")
 datoAnchoBanda.place(x=350, y=90)
 
-frecuenciaInicial = tk.Label(myframe, text="Frecuencia inicial:", font="Roboto 12", background="white")
+frecuenciaInicial = tk.Label(myframe, text="Frecuencia inicial:", font="Roboto 12", foreground="#233237", background="white")
 frecuenciaInicial.place(x=70, y=130)
 datoFrecuenciaInicial = tk.Label(myframe, text="xxxxxx", font="Roboto 12", background="white")
 datoFrecuenciaInicial.place(x=350, y=130)
 
-frecuenciaFinal = tk.Label(myframe, text="Frecuencia final:", font="Roboto 12", background="white")
+frecuenciaFinal = tk.Label(myframe, text="Frecuencia final:", font="Roboto 12", foreground="#024f94", background="white")
 frecuenciaFinal.place(x=70, y=170)
 datoFrecuenciaFinal = tk.Label(myframe, text="xxxxxx", font="Roboto 12", background="white")
 datoFrecuenciaFinal.place(x=350, y=170)
 
-NumeroParticiones = tk.Label(myframe, text="Número de particiones de frecuencia:", font="Roboto 12", background="white")
+NumeroParticiones = tk.Label(myframe, text="Número de particiones de frecuencia:", font="Roboto 12", foreground="#012e67", background="white")
 NumeroParticiones.place(x=70, y=210)
 datoNumeroParticiones = tk.Label(myframe, text="xxxxxx", font="Roboto 12", background="white")
 datoNumeroParticiones.place(x=350, y=210)
 
-NumeroTotalPeriodos = tk.Label(myframe, text="Número total de períodos:", font="Roboto 12", background="white")
+NumeroTotalPeriodos = tk.Label(myframe, text="Número total de períodos:", font="Roboto 12", foreground="#101629", background="white")
 NumeroTotalPeriodos.place(x=70, y=250)
 datoNumeroTotalPeriodos = tk.Label(myframe, text="xxxxxx", font="Roboto 12", background="white")
 datoNumeroTotalPeriodos.place(x=350, y=250)
 
-NumeroCapas = tk.Label(myframe, text="Número de capas de la estructura:", font="Roboto 12", background="white")
+NumeroCapas = tk.Label(myframe, text="Número de capas de la estructura:", font="Roboto 12", foreground="#10133a", background="white")
 NumeroCapas.place(x=70, y=310)
 datoNumeroCapas = tk.Label(myframe, text="xxxxxx", font="Roboto 12", background="white")
 datoNumeroCapas.place(x=350, y=310)
 
-capa = tk.Label(myframe, text="Capa n", font="Roboto 12 bold", background="white")
+capa = tk.Label(myframe, text="Capa n", font="Roboto 12 bold", foreground="#000e21", background="white")
 capa.place(x=70, y=350)
 
-tipoPerfil = tk.Label(myframe, text="Tipo de perfil para la capa n:", font="Roboto 12", background="white")
+tipoPerfil = tk.Label(myframe, text="Tipo de perfil para la capa n:", font="Roboto 12", foreground="#00202e",background="white")
 tipoPerfil.place(x=70, y=390)
 datoTipoPerfil = tk.Label(myframe, text="xxxxxx", font="Roboto 12", background="white")
 datoTipoPerfil.place(x=350, y=390)
@@ -111,10 +111,10 @@ datoNumeroParticiones.place(x=350, y=550)
 pregunta = tk.Label(myframe, text="¿Está seguro que desea realizar la simulación?", font="Roboto 14 bold", background="white")
 pregunta.place(x=35, y=610)
 
-buttonSi = tk.Button(myframe, text = "Si", font="Roboto 14 bold", cursor="hand2")
+buttonSi = tk.Button(myframe, text = "Si", font="Roboto 12 bold", cursor="hand2")
 buttonSi.place(x=200, y = 650)
 
-buttonNo = tk.Button(myframe, text = "No", font="Roboto 14 bold", cursor="hand2")
+buttonNo = tk.Button(myframe, text = "No", font="Roboto 12 bold", cursor="hand2")
 buttonNo.place(x=250, y = 650)
 
 
